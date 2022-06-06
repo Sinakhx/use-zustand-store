8b18a8f3d0e71b23ab6d4ab9cba2c93553deae86
Sina Khodabandehloo <khodabandehloo.sina@gmail.com>
2022-06-06 12:15:26 +0430

    fix: re-export immer middleware

--- lib/index.ts
+++ lib/index.ts
@@ -40,6 +40,7 @@ const useZustandStore = <T extends State>(storeFactory: StoreFactory<T>) => useZ
 // exports
 export { createContainer, getUntrackedObject, memo } from 'react-tracked';
 export { combine, devtools, persist, redux, subscribeWithSelector } from 'zustand/middleware';
+export { immer } from 'zustand/middleware/immer';
 export { default as shallow } from 'zustand/shallow';
 export { createZustandStore, useZustandStore, createTrackedSelector };
 export default create;
